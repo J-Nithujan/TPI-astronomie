0f7f2f297c668bd8d7adb2434fe50baf1ab5c785
JEGATHEESWARAN Nithujan <Nithujan.JEGATHEESWARAN@cpnv.ch>
2022-05-17 15:59:29 +0200

    database files update

--- data/extract-fk-values.ps1
+++ data/extract-fk-values.ps1
@@ -2,13 +2,14 @@
 # Project: TPI
 # Title: extract-fk-values
 # Author: Nithujan Jegatheeswaran
-# Version: 12.05.22
+# Version: 17.05.22
 ####################################
 
 Push-Location $PSScriptRoot 
 
 $properties= $("CONS", "TYPE", "Meilleur saison", "difficulté d'observation")
 foreach($key in $properties){
+        # Normalization is especially useful for accented character
         $normalized_key = $key.Normalize("FormD") -replace '\p{M}', ''
         Import-Csv -Path .\csv\LeCatalogueMessier.csv | Select-Object -Property $key -Unique | Export-Csv -path .\csv\$normalized_key.csv -Force -NoTypeInformation -Encoding UTF8
     }
--- data/get-pictures-csv.ps1
+++ data/get-pictures-csv.ps1
@@ -12,12 +12,12 @@ cd .\downthemall
 Get-ChildItem .\ | sort CreationTime | where { $_.Name -inotmatch '\S[0-9]+x[0-9]+\S' -and $_ -match '\S[0-9]+\S' } | Set-Variable 'list'
 
 # Copy file from /data to the directory used by the website
-$destination = 'C:\TPI\astronomy_app\app\static\assets\'
-foreach($item in $list) { Copy-Item $item -Destination $destination -Force}
+$destination = '..\astronomy_app\app\static\assets\'
+foreach($item in $list) { Copy-Item $item -Destination $destination -Force }
 
 # CSV file creation
-New-Item "C:\TPI\data\csv\pictures.csv" -Force
-$outfile = "C:\TPI\data\csv\pictures.csv"
+New-Item ".\csv\pictures.csv" -Force
+$outfile = ".\csv\pictures.csv"
 
 # write data in CSV 
 'M,PICTURE' | Out-File $outfile -Encoding utf8
