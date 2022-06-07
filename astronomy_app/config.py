8ffdd78e42ede5895e85b8d40d5fd8f13eb6682a
J-Nithujan <nithujan.jegatheeswaran@cpnv.ch>
2022-06-07 12:12:06 +0200

    registration success message update

--- astronomy_app/config.py
+++ astronomy_app/config.py
@@ -17,12 +17,12 @@ SQLALCHEMY_TRACK_MODIFICATIONS = True
 # Uncomment if commented and give correct credentials for local use
 # ------------
 # Encode the character to avoid error with some characters, especially useful for '@'
-# username = urllib.parse.quote('postgres')
-# password = urllib.parse.quote("P@$$w0rd")
-# SQLALCHEMY_DATABASE_URI = f'postgresql://{username}:{password}@localhost:5432/db_astronomy'
+username = urllib.parse.quote('postgres')
+password = urllib.parse.quote("P@$$w0rd")
+SQLALCHEMY_DATABASE_URI = f'postgresql://{username}:{password}@localhost:5432/db_astronomy'
 
 # Database link for Heroku, uncomment before deploying
 # ------------
 # Correct the url given by the heroku database server
-correct_db_uri = os.environ.get('DATABASE_URL').replace('postgres', 'postgresql')
-SQLALCHEMY_DATABASE_URI = correct_db_uri
+# correct_db_uri = os.environ.get('DATABASE_URL').replace('postgres', 'postgresql')
+# SQLALCHEMY_DATABASE_URI = correct_db_uri
